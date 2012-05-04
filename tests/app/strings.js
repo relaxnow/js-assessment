d35c68f02170643aa80b64f01c219ed05edce7f0
Boy Baukema <boy@ibuildings.nl>
2012-05-04 14:54:35 +0200

    Add some more tests (based on Douglas Crockfords 'On JavaScript' presentation)

---
+++ tests/app/strings.js
@@ -0,0 +1,46 @@
+define([ 'use!underscore' ], function (_) {
+  describe("strings", function () {
+    var fn, story;
+
+    beforeEach(function () {
+      fn = function() {};
+      story = 'There once was...';
+    });
+
+    it("you should know how to define a multi-line string safely", function(){
+      // Redefine story here
+
+      expect(story.split(/\r\n|\r|\n/).length).to.be.greaterThan(1);
+    });
+
+    it("you should know how to trim strings safely", function () {
+      // Make this work in IE7 (no need to fire up a VM, just dry code it and have it checked)
+      // Hint: look up String trim polyfill
+
+      expect(" trimmed ".trim()).to.be("trimmed");
+    });
+
+    it("you should know how to convert a number to a string", function() {
+      // Redefine fn here
+
+      expect(fn(19)).to.be("19");
+      expect(fn(null)).to.be(0);
+      expect(fn(undefined)).to.be(0);
+    });
+
+    it("you should know how to convert string to a number strictly", function() {
+      // Redefine fn here
+
+      expect(fn("9").to.be(9));
+      expect(isNaN(fn("99 bottles"))).to.be(true);
+    });
+
+    it("you should know how to convert string to a number loosely", function() {
+      // Redefine fn here
+
+      expect(fn("9").to.be(9));
+      expect(isNaN(fn("99 bottles"))).to.be(99);
+      expect(isNaN(fn("099 bottles"))).to.be(99);
+    });
+  });
+});
---
+++ tests/app/types.js
@@ -0,0 +1,58 @@
+define([ 'use!underscore' ], function (_) {
+  describe("types", function () {
+    var fn, rhino, rhino2;
+
+    beforeEach(function () {
+      fn = function() {};
+      rhino = {
+        feet: [1, 2, 3, 4],
+        owner: null,
+        male: '0'
+      };
+      rhino2 = {
+        feet: [1, 2, 3, 4],
+        owner: null,
+        male: '0'
+      };
+    });
+
+    it("you should be able to detect if something is an array", function () {
+      var result = 0;
+
+      // Fix this condition, make sure your solution also works in IE7
+      // Hint: look up array.isArray polyfill
+      if (typeof result.feet === 'Array') {
+        result = result.feet.length;
+      }
+
+      expect(result).to.be(4);
+    });
+
+    it("you should be able to detect if something is null", function() {
+      var result = 'captive';
+
+      // Fix this condition
+      if (!('owner' in rhino)) {
+        result = 'wild';
+      }
+
+      expect(result).to.be('wild')
+    });
+
+    it("you should be able to convert string values to booleans", function() {
+      var isFemale;
+
+      // Fix this comparison
+      // Note that this differs from PHP!
+      isFemale = !rhino.male;
+
+      expect(isFemale).to.be(true);
+    });
+
+    it("you should know how to compare objects", function(){
+      // Redefine fn here
+
+      expect(fn(rhino, rhino2)).to.be(true);
+    });
+  });
+});

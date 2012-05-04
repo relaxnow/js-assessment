1d7bb04a1dfdc755597fee729f431baa14c2c118
Boy Baukema <boy@ibuildings.nl>
2012-05-04 14:53:57 +0200

    Make objects into a simpler test, simply getting and setting keys

--- tests/app/objects.js
+++ tests/app/objects.js
@@ -1,60 +1,27 @@
-define([ 'use!underscore' ], function(_) {
-  describe("objects and context", function() {
-    var a, b, C, fn;
+define([ 'use!underscore' ], function (_) {
+  describe("objects", function () {
+    var person;
 
-    beforeEach(function() {
-      fn = function() {};
-
-      a = {
-        name : 'Matt',
-        greeting : 'Hello',
-        sayIt : function() {
-          return  this.greeting + ', ' +
-                  this.name + '!';
-        }
-      };
-
-      b = {
-        name : 'Rebecca',
-        greeting : 'Yo'
-      };
-
-      C = function(name) {
-        this.name = name;
-        return this;
-      };
+    beforeEach(function () {
+      person = { name:"John Doe", firstName:"John", surname:"Doe", twitter:"https://twitter.com/JohnDoe" };
     });
 
-    it("you should be able to alter the context in which a method runs", function() {
-      // define a function for fn so that the following will pass
-      expect(fn()).to.be('Yo, Rebecca!');
+    it("you should be able to remove properties from an object", function () {
+      // Modify the person object here
+
+      expect(typeof person.twitter).to.be('undefined');
     });
 
-    it("you should be able to alter multiple objects at once", function() {
-      // define a function for fn so that the following will pass
-      var obj1 = new C('Rebecca'),
-          obj2 = new C('Melissa'),
-          greeting = "What's up";
+    it("you should be able to set a propertyname with a space in it", function () {
+      // Modify the person object here
 
-      fn(greeting);
-
-      expect(obj1.greeting).to.be(greeting);
-      expect(obj2.greeting).to.be(greeting);
-      expect(new C('Ellie').greeting).to.be(greeting);
+      expect(person['bank number']).to.be('1010101');
     });
 
-    it("you should be able to iterate over an object's 'own' properties", function() {
-      // define a function for fn so that the following will pass
-      var C = function() {
-        this.foo = 'bar';
-        this.baz = 'bim';
-      };
+    it("you should be able to set a propertyname with a reserved word", function () {
+      // Modify the person object here
 
-      C.prototype.bop = 'bip';
-
-      var obj = new C();
-
-      expect(fn(obj)).to.eql([ 'foo: bar', 'baz: bim' ]);
+      expect(person['this']).to.be(person);
     });
   });
 });

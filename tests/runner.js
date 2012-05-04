7de5ae7aae02387ecaeeceb4fc3550336775e5fe
Boy Baukema <boy@ibuildings.nl>
2012-05-04 14:55:21 +0200

    Change the order of the tests to match more closely to Douglas Crockfords 'On JavaScript' videos

--- tests/runner.js
+++ tests/runner.js
@@ -1,12 +1,17 @@
 var tests = [
   // link to test files here
-
+  'order!tests/app/objects',
+  'order!tests/app/numbers',
+  'order!tests/app/math',
+  'order!tests/app/strings',
   'order!tests/app/arrays',
-  'order!tests/app/async',
+  'order!tests/app/dates',
+  'order!tests/app/regexps',
+  'order!tests/app/types',
   'order!tests/app/flowControl',
   'order!tests/app/functions',
   'order!tests/app/modules',
-  'order!tests/app/objects'
+  'order!tests/app/async'
 ];
 
 require(tests, function() {
